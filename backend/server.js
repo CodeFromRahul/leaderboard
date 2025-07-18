b666be39b69640e16a0386fb426d7ffc82412c4f
CodeFromRahul <rahulvish194002@gmail.com>
2025-07-18 12:55:28 +0530

    Added some changes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
 import userRoutes from './routes/userRoutes.js';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
